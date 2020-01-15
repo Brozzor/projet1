52b3f502c84bd1bfd5bee58b19e7274c1cd57cfd
Brozzor <46285168+Brozzor@users.noreply.github.com>
2020-01-15 23:26:20 +0100

    init
    premier commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+      width: 1920, 
+      height: 1080,
+      icon: `file://${__dirname}/assets/icon.png`,
+      title: 'South Park by Romain et Anthon',
+      movable: true,
+      frame: false,
+      backgroundColor: "#111"
+    });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  })
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ main.js
@@ -0,0 +1,73 @@
+let game = new Phaser.Game(600, 600);
+let speed = 300;
+let southPark = {
+    preload: function() {
+        game.load.image('background2', 'assets/fond.png');
+        game.load.image('background1', 'assets/bg1.png');
+        game.load.image('background3', 'assets/bg3.png');
+        game.load.image('player', 'assets/player.png');
+        game.load.image('badguy', 'assets/kyle.png');
+    },
+    create: function() {
+
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        test = 1;
+        game.add.sprite(0, 0, "background" + entierAleatoire(1, 3));
+
+        this.player = game.add.sprite(300, 500, 'player');
+        this.player.anchor.set(0.5);
+        game.physics.arcade.enable(this.player);
+        this.cursors = game.input.keyboard.createCursorKeys();
+
+        this.badGuys = game.add.group();
+
+        this.timer = game.time.events.loop(200, this.addBadGuy, this);
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", {font: "30px Arial", fill: "#fff"})
+    },
+    update: function() {
+        game.physics.arcade.overlap(this.player, this.badGuys, this.restartGame, null, this);
+        this.player.body.velocity.x = 0;
+        this.player.body.velocity.y = 0;
+        if (this.cursors.left.isDown){
+            this.player.body.velocity.x = -speed;
+        }   
+        if (this.cursors.right.isDown){
+            this.player.body.velocity.x = speed;
+        }
+        if (this.cursors.up.isDown){
+            this.player.body.velocity.y = -speed;
+        }   
+        if (this.cursors.down.isDown){
+            this.player.body.velocity.y = speed;
+        }
+        if (this.player.inWorld == false){
+            this.restartGame();
+        }
+        
+    },
+    restartGame: function(){
+        game.state.start('southPark');
+    },
+    addBadGuy: function(){
+        let position = Math.floor(Math.random() * 550) + 1;
+        let badGuy = game.add.sprite(position, -50, 'badguy');
+        game.physics.arcade.enable(badGuy);
+        badGuy.body.gravity.y = 200;
+
+        this.badGuys.add(badGuy);
+
+        this.score += 20;
+        this.labelScore.text = this.score;
+
+        badGuy.checkWorldBounds = true;
+        badGuy.outOfBoundsKill = true;
+    }
+};
+
+function entierAleatoire(min, max)
+    {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+game.state.add('southPark', southPark);
+game.state.start('southPark');
