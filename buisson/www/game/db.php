3003a6ae995fed700eb821c760bb60cb7744e1d7
Brozzor <46285168+Brozzor@users.noreply.github.com>
2020-02-04 18:16:51 +0100

    test ajax

--- buisson/www/game/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo = new PDO('mysql:dbname=game_sp;host=164.132.46.160', 'backend', 'Avq3yjLDAsW8pzNr');
-//$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
