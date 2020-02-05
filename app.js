19d3990a726991555b612ae11571b17da0d862c9
Brozzor <46285168+Brozzor@users.noreply.github.com>
2020-02-05 19:55:58 +0100

    ajout de la connection à la bdd mysql

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 var http = require('http');
 var fs = require('fs');
 let express = require('express');
+let mysql = require('mysql');
+
 let app = express();
+let conn = mysql.createConnection({
+  database: 'game_sp',
+  host: "164.132.46.160",
+  user: "backend",
+  password: "Avq3yjLDAsW8pzNr"
+});
+ 
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log("Connecter à la bdd!");
+}); 
+
 app.use('/assets', express.static('assets'))
-var server = http.createServer(app);
+
+let server = http.createServer(app);
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+'/main.html')
 });
 
 
-var io = require('socket.io').listen(server);
+let io = require('socket.io').listen(server);
 
 
 io.sockets.on('connection', function (socket) {
@@ -21,6 +36,16 @@ io.sockets.on('connection', function (socket) {
     });	
     socket.on('pseudo', function (pseudo) {
         console.log('son pseudo est ' + pseudo);
+        let sql = `SELECT count(*) as nb FROM user WHERE pseudo = '${pseudo}'`;
+        conn.query(sql, function(err, rows, fields) {
+            if (err) throw err;
+               if (rows[0].nb == '0')
+               {
+                let sql = `INSERT INTO user(pseudo,money) VALUES('${pseudo}','0')`;
+                conn.query(sql);
+               }
+            
+        });
     });	
 
 });
